62a22ffa6b577e4d87e1535b73c1882553e1a02b
Your Name <your-email@example.com>
2025-01-12 13:04:46 +0530

    user authntication done

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {body} = require('express-validator');
 const userController = require('../controllers/user.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/register', [
     body('fullname.firstname').isLength({min: 3}).withMessage('first name must be at least 3 characters long'),
@@ -10,6 +11,17 @@ router.post('/register', [
     body('password').isLength({min: 6}).withMessage('password must be at least 6 characters long')
 ],
  userController.registerUser
-);
+)
+
+router.post('/login',[
+    body('email').isEmail().withMessage('email is invalid'),
+    body('password').isLength({min: 6}).withMessage('password must be at least 6 characters long')
+],
+userController.loginUser
+)
+
+router.get('/profile', authMiddleware.authUser, userController.getUserProfile);
+
+router.get('/logout',authMiddleware.authUser,userController.logoutUser);
 
 module.exports = router;
