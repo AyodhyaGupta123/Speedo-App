e87a53872da7c1c988881287da0b2ab5d7c0296a
Your Name <your-email@example.com>
2025-01-12 11:34:59 +0530

    User registered successfully

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+        minlength: [3,'first name must be at least 3 characters long']
+    },
+    lastname: {
+        type: String,
+        minlength: [3,'last name must be at least 3 characters long']
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    socketId: {
+        type: String,
+        default: null
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({_id: this._id}, process.env.JWT_SECRET);
+    return token;
+}
+
+userSchema.methods.comparePassword = async function(password) {
+   return await bcrypt.compare(password, this.password);
+}
+
+userSchema.statics.hashPassword = async function(password) {
+    return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.model('user', userSchema); 
+
+module.exports = userModel;
